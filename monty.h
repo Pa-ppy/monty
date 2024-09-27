362d7f664a0bfa0bedad45ccb56da03cbcc5a9f0
Pa-ppy <rayjacks07@gmail.com>
2024-09-27 13:33:38 -0700

    Added Header file

---
+++ monty.h
@@ -0,0 +1,26 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number, char *arg);
+} instruction_t;
+
+/* Function prototypes */
+void push(stack_t **stack, unsigned int line_number, char *arg);
+void pall(stack_t **stack, unsigned int line_number);
+void free_stack(stack_t *stack);
+
+#endif
