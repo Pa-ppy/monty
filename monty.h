39b08b48cc8262ae571714c5e2e543ed96a4bd22
Pa-ppy <rayjacks07@gmail.com>
2024-09-27 21:07:35 -0700

    Added Header file

--- monty.h
+++ monty.h
@@ -5,6 +5,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * struct stack_s - Doubly linked list representation of a stack
+ * @n: integer
+ * @prev: points to the previous element of the stack
+ * @next: points to the next element of the stack
+ *
+ * Description: doubly linked list node structure for stacks, queues
+ */
 typedef struct stack_s
 {
 	int n;
@@ -12,6 +20,13 @@ typedef struct stack_s
 	struct stack_s *next;
 } stack_t;
 
+/**
+ * struct instruction_s - opcode and its functions
+ * @opcode:the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function for stack, queues
+ */
 typedef struct instruction_s
 {
 	char *opcode;
@@ -22,5 +37,14 @@ typedef struct instruction_s
 void push(stack_t **stack, unsigned int line_number, char *arg);
 void pall(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *stack);
+
+/* Main program function */
+void run_monty(FILE *file);
+
+/* Error handling functions */
+void error_usage(void);
+void error_file(char *filename);
+void error_malloc(void);
+void error_instruction(unsigned int line_number, char *opcode);
 
 #endif
