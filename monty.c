5640f5277aa25b419593e0b44bb2ea6fab9f85e4
Pa-ppy <rayjacks07@gmail.com>
2024-09-27 21:07:59 -0700

    monty

--- monty.c
+++ monty.c
@@ -2,15 +2,35 @@
 
 /**
  * main - Entry point of program
- * @file: File for usage
- * @argc: Argument
- * @argv: Argument pool
+ * @argc: Argument count
+ * @argv: Argument vector
  *
- * Return: 0 on success
+ * Return: 0 on success, or EXIT_FAILURE on failure
  */
 int main (int argc, char *argv[])
 {
 	FILE *file;
+
+	if (argc != 2)
+		error_usage();
+
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+		error_file(argv[1]);
+
+	run_monty(file);
+
+	fclose(file);
+
+	return (0);
+}
+
+/**
+ * run_monty - Reads and executes Monty bytecode commands
+ * @file: Pointer to the file containing monty bytecode
+ */
+void run_monty(FILE *file)
+{
 	char *line = NULL, *opcode = NULL, *arg = NULL;
 	size_t len = 0;
 	unsigned int line_number = 0;
@@ -20,19 +40,6 @@ int main (int argc, char *argv[])
 		{"pall", pall},
 		{NULL, NULL}
 	};
-
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-
-	file = fopen(argv[1], "r");
-	if (file == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
 
 	while (getline(&line, &len, file) != -1)
 	{
@@ -54,18 +61,9 @@ int main (int argc, char *argv[])
 			i++;
 		}
 		if (instructions[i].opcode == NULL)
-		{
-			fprint(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
-			free(line);
-			fclose(file);
-			free_stack(stack);
-			exit(EXIT_FAILURE);
-		}
+			error_instruction(line_number, opcode);
 	}
 
 	free(line);
-	fclose(file);
 	free_stack(stack);
-
-	return (0);
 }
