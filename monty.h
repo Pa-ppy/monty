0fd67dad5cddf3f48ec2ed453a9dc1150475ec8a
Pa-ppy <rayjacks07@gmail.com>
2024-09-27 22:19:34 -0700

    Added Header file

--- monty.h
+++ monty.h
@@ -34,8 +34,12 @@ typedef struct instruction_s
 } instruction_t;
 
 /* Function prototypes */
+
+/* Opcode functions */
 void push(stack_t **stack, unsigned int line_number, char *arg);
-void pall(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number, char *arg);
+
+/* Utility function */
 void free_stack(stack_t *stack);
 
 /* Main program function */
