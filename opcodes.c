72e08ae8932352f8ce18bcc6b90ec396b9a0320b
Pa-ppy <rayjacks07@gmail.com>
2024-09-27 21:09:20 -0700

    opcodes

--- opcodes.c
+++ opcodes.c
@@ -19,10 +19,7 @@ void push(stack_t **stack, unsigned int line_number, char *arg)
 
 	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
+		error_malloc();
 
 	new_node->n = n;
 	new_node->prev = NULL;
